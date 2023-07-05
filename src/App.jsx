c38faa9e6525458b497c5c6a2765174d68dc6a7e
yumelih <yumelih@gmail.com>
2023-07-05 21:38:46 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+
+import Comment from "./components/Comment";
+// import iconDelete from "./assets/icon-delete.svg";
+
+function App() {
+  return (
+    <>
+      <Comment />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Comment.jsx
@@ -0,0 +1,9 @@
+function Comment() {
+  return (
+    <div>
+      <p className="at">WARRR</p>
+    </div>
+  );
+}
+
+export default Comment;
